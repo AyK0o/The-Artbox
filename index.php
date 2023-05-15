6a767ff3380caae4fa32aac776d45270aeb9eb77
AyK0o <merlingalland@gmail.com>
2023-05-15 09:10:22 +0200

    Text oeuvres fix

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="fr">
+    <?php 
+    include("oeuvres.php");
+    ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>The ArtBox</title>
+</head>
+<body>
+    <header>
+        <?php include ("header.php"); ?>
+    </header>
+    <main>
+        <div id="liste-oeuvres">
+            <?php
+            AfficherToutOeuvres($oeuvres,); 
+            ?>
+    </div>
+    </main>
+    <footer>
+        <?php include ("footer.php"); ?>
+    </footer>
+</body>
+</html>
